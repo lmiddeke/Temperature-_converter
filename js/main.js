f675f3c87255d4bae98e296f569158a2ae7f1d3b
Leonie Middeke <86887032+lmiddeke@users.noreply.github.com>
2022-03-24 18:52:53 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Write your pseduo code first! 
+
+//0->32
+//box for entering a temp
+
+//press button to click to activate smurfs
+//displays conversion
+
+document.quersySelector('#check').addEventListener('click, convert')
+
+function concert() {
+    
+
+}
